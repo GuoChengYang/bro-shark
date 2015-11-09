847fa6f1772d21447d953f2af47395473b165df2
blacktop <blacktop@users.noreply.github.com>
2015-11-08 21:53:42 -0700

    add upload UI

--- web/config.py
+++ web/config.py
@@ -4,25 +4,27 @@
 import os
 
 
-# class BaseConfig(object):
-#     SECRET_KEY = os.environ['SECRET_KEY']
-#     DEBUG = os.environ['DEBUG']
-#     DB_NAME = os.environ['DB_NAME']
-#     DB_USER = os.environ['DB_USER']
-#     DB_PASS = os.environ['DB_PASS']
-#     DB_SERVICE = os.environ['DB_SERVICE']
-#     DB_PORT = os.environ['DB_PORT']
-#     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
-#         DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
-#     )
-
-
 class BaseConfig(object):
-    SECRET_KEY = 'hi'
-    DEBUG = True
-    DB_NAME = 'postgres'
-    DB_SERVICE = 'localhost'
-    DB_PORT = 5432
-    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}/{2}'.format(
-        DB_SERVICE, DB_PORT, DB_NAME
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'secret'
+    DEBUG = os.environ.get('DEBUG') or True
+    DB_NAME = os.environ.get('DB_NAME') or 'postgres'
+    DB_USER = os.environ.get('DB_USER') or 'postgres'
+    DB_PASS = os.environ.get('DB_PASS') or 'postgres'
+    DB_SERVICE = os.environ.get('DB_SERVICE') or 'localhost'
+    DB_PORT = os.environ.get('DB_PORT') or 5432
+    SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}@{2}:{3}/{4}'.format(
+        DB_USER, DB_PASS, DB_SERVICE, DB_PORT, DB_NAME
     )
+    # SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}/{2}'.format(
+    #     DB_SERVICE, DB_PORT, DB_NAME
+    # )
+
+# class BaseConfig(object):
+#     SECRET_KEY = 'hi'
+#     DEBUG = True
+#     DB_NAME = 'postgres'
+#     DB_SERVICE = 'localhost'
+#     DB_PORT = 5432
+#     SQLALCHEMY_DATABASE_URI = 'postgresql://{0}:{1}/{2}'.format(
+#         DB_SERVICE, DB_PORT, DB_NAME
+#     )
